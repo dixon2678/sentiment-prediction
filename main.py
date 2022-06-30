f6aaa6e7eebadc0ba7037d39339952abdb290549
Dixon <81808129+dixon2678@users.noreply.github.com>
2022-07-01 03:43:57 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,81 @@
+import nltk
+from nltk.corpus import stopwords
+import pandas as pd
+import fsspec
+import gcsfs
+import nltk
+from google.cloud import storage
+from google.oauth2 import service_account
+import os
+from sklearn.metrics import auc, accuracy_score, confusion_matrix, mean_squared_error, f1_score
+from sklearn.model_selection import cross_val_score, GridSearchCV, KFold, RandomizedSearchCV, train_test_split
+import xgboost as xgb
+from datetime import datetime
+import pickle
+from google.cloud import bigquery
+from sklearn.feature_extraction.text import CountVectorizer
+from nltk.sentiment import SentimentIntensityAnalyzer
+from nltk.stem.porter import *
+nltk.download('punkt')
+nltk.download('vader_lexicon')
+nltk.download('stopwords')
+import string
+
+gcp_json_credentials_dict = json.loads(os.environ['creds'])
+credentials = service_account.Credentials.from_service_account_info(gcp_json_credentials_dict)
+client = storage.Client(credentials=credentials, project='final-347314')
+bucket = client.get_bucket('csv-etl-fyp')
+date = datetime.today().strftime('%Y-%m-%d')
+
+def load_ml_model(self):
+  blob = bucket.blob('model/ml/xgb-base.pkl')
+  blob.download_to_filename('loaded-xgb.pkl')
+  loaded_file = open('loaded-xgb.pkl', 'rb')
+  model = pickle.load(loaded_file)
+  return model
+
+def load_prediction_data(self):
+  client = bigquery.Client(credentials=credentials, project='final-347314')
+  sql = """
+  SELECT * FROM `final-347314.main.rss_coin_name`
+  """
+  df = client.query(sql).to_dataframe()
+  return df
+
+def load_vectorizer(self):
+  blob = bucket.blob('model/cv/cv.pkl')
+  blob.download_to_filename('cv.pkl')
+  loaded_file = open('cv.pkl', 'rb')
+  cv = pickle.load(loaded_file)
+  return cv
+
+def preprocessing_pipeline(self, data, cv):
+
+  data = data.dropna()
+  data = [x.lower() for x in data]
+
+  st = PorterStemmer()
+  data = [st.stem(x) for x in data]
+
+  stop = set(stopwords.words('english'))
+  data = [t for t in data if t not in stop and t not in string.punctuation]
+  data = cv.transform(data)
+  return data
+
+
+def upload_to_gcs(self, path):
+  
+  blob = bucket.blob('model/prediction/prediction' + date + '.csv')
+  blob.upload_from_filename(path)
+
+
+if __name__ == "__main__":
+  model = load_ml_model()
+  df = load_prediction_data()
+  cv = load_vectorizer()
+  final_df = preprocessing_pipeline(df['title'], cv)
+  pred = model.predict(final_df)
+  df['pred'] = pred
+  path = 'prediction' + date + '.csv'
+  df.to_csv(path)
+  upload_to_gcs(path)
